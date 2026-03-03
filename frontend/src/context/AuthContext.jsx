164bffa543ba4694972e26cca4596788b49603a4
SunRoute <sunroute@proton.com>
2026-03-03 16:00:46 +0100

    feat: AuthContext improvement with jwt-decode

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
+import jwtDecode from "jwt-decode";
 
 // Crear un contexto para manejar el estado de autenticación
 const AuthContext = createContext();
@@ -8,10 +9,28 @@ export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [user, setUser] = useState(null);
 
-  // Almacenar el token en localStorage cuando se inicia sesión
-  const login = (token) => {
-    localStorage.setItem("token", token);
-    setToken(token);
+  // Decodificar token y actualizar el estado cuando cambie
+  useEffect(() => {
+    if (token) {
+      try {
+        const decoded = jwtDecode(token);
+        setUser({
+          id: decoded.id,
+          role: decoded.role,
+        });
+      } catch (error) {
+        console.error("Token inválido");
+        logout();
+      }
+    } else {
+      setUser(null);
+    }
+  }, [token]);
+
+  // Almacenar el token en localStorage cuando se inicie sesión
+  const login = (newToken) => {
+    localStorage.setItem("token", newToken);
+    setToken(newToken);
   };
 
   // Limpiar el token de localStorage cuando se cierre la sesión
@@ -27,7 +46,7 @@ export const AuthProvider = ({ children }) => {
   return (
     // Devolver el contexto y el componente hijo
     <AuthContext.Provider
-      value={{ token, user, setUser, login, logout, isAuthenticated }}
+      value={{ token, user, login, logout, isAuthenticated }}
     >
       {children}
     </AuthContext.Provider>
