1eb20ab8e36469289d4efa9bd9cdf53a3bee0fce
SunRoute <sunroute@proton.me>
2026-03-09 19:29:29 +0100

    feat: layout links by role

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 // Outlet es donde se renderizará el componente que se encuentra en la ruta hija
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import "./MainLayout.css";
 
 const MainLayout = () => {
   const { user, logout } = useAuth();
   const [menuOpen, setMenuOpen] = useState(false);
+  const [adminOpen, setAdminOpen] = useState(false);
+  const toggleAdminMenu = () => {
+    setAdminOpen(!adminOpen);
+  };
 
   return (
     <div className="layout">
@@ -22,20 +26,31 @@ const MainLayout = () => {
       </header>
 
       <aside className={`layout_sidebar ${menuOpen ? "active" : ""}`}>
-        {user?.role === "admin" && (
-          <>
-            <p>Dashboard</p>
-            <p>Usuarios</p>
-            <p>Horas Extra</p>
-          </>
-        )}
+        <nav>
+          <NavLink to="/dashboard">Dashboard</NavLink>
 
-        {user?.role === "employee" && (
-          <>
-            <p>Mi Jornada</p>
-            <p>Mis Horas Extra</p>
-          </>
-        )}
+          <NavLink to="/my-workday">Mi jornada</NavLink>
+
+          <NavLink to="/my-overtimes">Mis horas extra</NavLink>
+
+          {user?.role === "admin" && (
+            <>
+              <button className="sidebar-section" onClick={toggleAdminMenu}>
+                Administración
+              </button>
+
+              {adminOpen && (
+                <div className="submenu">
+                  <NavLink to="/admin">Panel admin</NavLink>
+
+                  <NavLink to="/users">Usuarios</NavLink>
+
+                  <NavLink to="/overtime-management">Horas extra</NavLink>
+                </div>
+              )}
+            </>
+          )}
+        </nav>
       </aside>
 
       <main className="layout_content">
